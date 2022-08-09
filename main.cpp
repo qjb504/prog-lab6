684083a42bf9cd5b3158776c82b4082521153c74
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-09 16:04:18 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include "mbed.h"
 
+DigitalOut dir1(D2);
+DigitalOut dir2(D3);
+PwmOut pwm(D4);
+
+void setMotorSpeed(float speed) {
+    if (speed > 0) {
+        dir1 = true;
+        dir2 = false;
+        pwm = speed;
+    } else {
+        dir1 = false;
+        dir2 = true;
+        pwm = -speed;
+    }
+}
+
 int main() {
     while (true) {
-        sleep();
+        setMotorSpeed(1.0);
+        thread_sleep_for(1000);
+        setMotorSpeed(0.0);
+        thread_sleep_for(1000);
+        setMotorSpeed(-0.5);
+        thread_sleep_for(1000);
+        setMotorSpeed(0);
+        thread_sleep_for(1000);
     }
 }
